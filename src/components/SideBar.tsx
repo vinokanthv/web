90894f658281379670a5e0d178f5e49217c615f8
Vinokanth Velu <vinokanth.velu@gmail.com>
2025-09-16 23:57:51 +0100

    Add hover effect to menu bar item

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React from 'react';
-import {Box, HStack, Link, VStack} from "@chakra-ui/react";
+import {Badge, Box, Button, HStack, IconButton, List, ListItem, Spacer, Text} from "@chakra-ui/react";
 import NextLink from "next/link";
 import {GoHomeFill} from "react-icons/go";
 import {LuWorkflow} from "react-icons/lu";
 import {MdOutlineHistory} from "react-icons/md";
 import {MAGENTA_COLOR} from "@/utils/colors";
+import {LiaAtomSolid} from "react-icons/lia";
+import {IoIosArrowBack, IoIosArrowForward} from "react-icons/io";
 
 type SideBarMenuItemProps = {
     label: string;
@@ -14,28 +16,71 @@ type SideBarMenuItemProps = {
     icon: React.ReactNode;
 }
 
+function LogoAndName() {
+
+    return (
+        <>
+            <Box height="50px" flexDirection="row" display="flex" justifyContent="space-between">
+                <HStack>
+                    <LiaAtomSolid size="30px" color="blue"/>
+                    <Badge size='lg' variant='solid' background="blue">
+                        <Text textStyle="lg">Mgaenta.ai</Text>
+                    </Badge>
+                    <Spacer/>
+                    <SideBarCollapseControl/>
+                </HStack>
+            </Box>
+        </>
+    );
+
+}
+
 function SideBarMenuItem(props: SideBarMenuItemProps) {
     const {label, href, icon} = props;
 
-    return <HStack align="start" p="5px">
-        <Box width="3px"></Box>
-        {icon}
-        <Link variant="plain" as={NextLink} href={href} color="black">{label}</Link>
+    return <ListItem key={href} color="black">
+        <NextLink href={href}>
+            <Button w="180px" justifyContent="flex-start" variant="ghost"
+                    _hover={{bg: "lightgrey", color: "magenta"}}><HStack>
+                {icon}
+                <Text textStyle="sm">{label}</Text>
+            </HStack>
+            </Button>
+        </NextLink>
+    </ListItem>
+}
+
+function SideBarCollapseControl() {
+    const [open, setOpen] = React.useState(false);
+
+    return <HStack justify="end">
+        <IconButton size="md" variant="ghost" onClick={() => setOpen(!open)}>
+            {icon()}
+        </IconButton>
     </HStack>;
+
+    function icon(): React.ReactNode {
+        if (open) {
+            return <IoIosArrowBack size="25px" color="blue"/>
+        }
+
+        return <IoIosArrowForward size="25px" color="blue"/>;
+    }
 }
 
 function SideBar() {
     return (
         <>
-            <Box width="200px"
+            <Box width="220px"
                  height="100vh"
                  bg="gray.100"
                  color="white"
                  p={4}
                  position="fixed"
+                 display="flex"
             >
-                <VStack align="stretch">
-                    <Box height="50px"/>
+                <List.Root color="white" fontSize="14" variant="plain" gap={3}>
+                    <LogoAndName />
                     <SideBarMenuItem icon={<GoHomeFill color={MAGENTA_COLOR} size="25px"/>} label="Home" href="/"/>
 
                     <SideBarMenuItem icon={<MdOutlineHistory color={MAGENTA_COLOR} size="25px"/>}
@@ -43,11 +88,14 @@ function SideBar() {
                                      href="/history"/>
 
                     <SideBarMenuItem icon={<LuWorkflow color={MAGENTA_COLOR} size="25px"/>} label="Workflow"
-                                     href="/workflow"/>
-                </VStack>
+                                     href="/workflows"/>
+
+                </List.Root>
             </Box>
         </>
     );
+
+
 }
 
 export default SideBar;
